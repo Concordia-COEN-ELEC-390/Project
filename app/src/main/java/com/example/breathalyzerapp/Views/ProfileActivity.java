625789fd6c855a18f158439a48fea466b56ba708
Anthony <anthonytkalec@gmail.com>
2022-03-05 18:57:13 -0500

    Fixed all merge conflicts, sprint1 demo ready

--- app/src/main/java/com/example/breathalyzerapp/Views/ProfileActivity.java
+++ app/src/main/java/com/example/breathalyzerapp/Views/ProfileActivity.java
@@ -1,8 +1,10 @@
-package com.example.breathalyzerapp;
+package com.example.breathalyzerapp.Views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.breathalyzerapp.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
